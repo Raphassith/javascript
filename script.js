7a1f0989763e5c59aba2f3880dd5e2fcbdc4a608
รภัสสิทธิ์ กำจรอัครหิรัญ <34786940+Raphassith@users.noreply.github.com>
2025-04-19 02:18:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,25 @@ function sortArray(){
     points.sort((a, b)=> {return b - a});
     document.getElementById("demo2").innerHTML = points;
 }
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
